a4c034a44381b4283d60d219f534e9c94e131d98
dlather <deepaklather2709@gmail.com>
2025-01-10 16:21:52 +0530

    configure lowdb

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello World');
+import { JSONFilePreset } from 'lowdb/node';
+
+type DbSchema = {
+  posts: string[];
+};
+
+const defaultData: DbSchema = { posts: [] };
+
+async function main() {
+  const db = await JSONFilePreset<DbSchema>('db.json', defaultData);
+  console.log(db.data);
+  db.data.posts.push('hello world');
+  await db.write();
+}
+
+main();
